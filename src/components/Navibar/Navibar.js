8be50da258852adcc93f9857c760d12709bcb781
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-10-10 22:12:26 +0300

    add dropdown to shedule menu, add login form

--- src/components/Navibar/Navibar.js
+++ src/components/Navibar/Navibar.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Container } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Button,
+  Modal,
+  Form,
+  NavDropdown,
+} from "react-bootstrap";
 import styled from "styled-components";
 
 const Styles = styled.div`
@@ -17,6 +25,11 @@ const Styles = styled.div`
 `;
 
 function Navibar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <Styles>
@@ -24,7 +37,10 @@ function Navibar() {
           <Container>
             <Navbar.Brand>SiteName</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
+            <Navbar.Collapse
+              id="responsive-navbar-nav"
+              className="justify-content-between"
+            >
               <Nav className="mr-auto">
                 <Nav.Link>
                   <Link to="/">Main</Link>
@@ -38,11 +54,75 @@ function Navibar() {
                 <Nav.Link>
                   <Link to="/team">Team</Link>
                 </Nav.Link>
+
+                <NavDropdown title="Schedule" id="basic-nav-dropdown">
+                  <NavDropdown title="Schedule 1" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action1
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Action2
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Action3
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Schedule 2" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action1
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Action2
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Action3
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Schedule 3" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">
+                      Action1
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">
+                      Action2
+                    </NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">
+                      Action3
+                    </NavDropdown.Item>
+                  </NavDropdown>
+                </NavDropdown>
               </Nav>
+              <Button variant="primary" onClick={handleShow}>
+                Log in
+              </Button>
             </Navbar.Collapse>
           </Container>
         </Navbar>
       </Styles>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Log in</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group comtrolId="formBasicEmail">
+              <Form.Label>Email Adress</Form.Label>
+              <Form.Control type="email" placeholder="Enter your Email" />
+            </Form.Group>
+            <Form.Group comtrolId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Enter Password" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -6,28 +6,30 @@ function Slider() {
   return (
     <>
       <Carousel>
-        <Carousel.Item style={{ height: "600px" }}>
+        <Carousel.Item style={{ height: "300px" }}>
           <img className="d-block w-100" src={img_1} alt="first_slide" />
           <Carousel.Caption>
-            <h3>First slide</h3>
-            <p>Veniam eu enim culpa et non laboris irure nulla.</p>
+            <h3 style={{ color: "black" }}>First slide</h3>
+            <p style={{ color: "black" }}>
+              Veniam eu enim culpa et non laboris irure nulla.
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item style={{ height: "600px" }}>
-          <img className="d-block w-100" src={img_1} alt="first_slide" />
+        <Carousel.Item style={{ height: "300px" }}>
+          <img className="d-block w-100" src={img_1} alt="second_slide" />
           <Carousel.Caption>
-            <h3>Second slide</h3>
-            <p>
+            <h3 style={{ color: "black" }}>Second slide</h3>
+            <p style={{ color: "black" }}>
               Minim non ea ullamco sint mollit incididunt veniam amet commodo
               adipisicing veniam qui sit.
             </p>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item style={{ height: "600px" }}>
-          <img className="d-block w-100" src={img_1} alt="first_slide" />
+        <Carousel.Item style={{ height: "300px" }}>
+          <img className="d-block w-100" src={img_1} alt="third_slide" />
           <Carousel.Caption>
-            <h3>Third slide</h3>
-            <p>
+            <h3 style={{ color: "black" }}>Third slide</h3>
+            <p style={{ color: "black" }}>
               Minim Lorem occaecat id nostrud deserunt dolor nisi incididunt.
             </p>
           </Carousel.Caption>
