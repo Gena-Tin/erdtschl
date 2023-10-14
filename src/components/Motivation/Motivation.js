892844d9b7e108cc1e41a9f0fad3ee667a3bfd3a
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2023-10-14 20:41:03 +0300

    refactored motivation component

--- src/components/Motivation/Motivation.js
+++ src/components/Motivation/Motivation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import Counter from "../Counter/Counter";
 import {
   BsFillPeopleFill,
   BsFillMortarboardFill,
@@ -7,68 +8,53 @@ import {
   BsFillTrophyFill,
   BsDashLg,
 } from "react-icons/bs";
-import Counter from "../Counter/Counter";
+
+const cardStyles = {
+  display: "flex",
+  flexDirection: "column",
+  padding: "2rem",
+  alignItems: "center",
+  background: "rgba(0,0,0,0.05)",
+  border: "2px solid rgba(0, 0, 0, 0.3)",
+  borderRadius: "1rem",
+  boxShadow: "1px 17px 24px -13px rgba(0,0,0,0.66)",
+  webkitBoxShadow: "1px 17px 24px -13px rgba(0,0,0,0.66)",
+  mozBoxShadow: "1px 17px 24px -13px rgba(0,0,0,0.66)",
+};
+
+const icons = [
+  <BsFillPeopleFill size={40} />,
+  <BsFillTrophyFill size={40} />,
+  <BsFillMortarboardFill size={40} />,
+  <BsFillHandThumbsUpFill size={40} />,
+];
+
+const title = "Qui enim reprehenderit enim esse in et laborum";
+const numbers = [1430, 7900, 30, 20];
+const texts = ["enim esse", "enim esse", "enim esse", "enim esse"];
 
 function Motivation() {
   return (
-    <>
-      <Container style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
-        <h2 style={{ textAlign: "center", marginBottom: "2rem" }}>
-          Qui enim reprehenderit enim esse in et laborum
-        </h2>
-        <Row style={{ fontSize: "2rem" }}>
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              margin: "auto",
-            }}
-          >
-            <BsFillPeopleFill size={40} />
-            <BsDashLg size={40} />
-            <Counter number={1430} /> {/*число на счетчике */}
-            <span>enim esse</span>
+    <Container
+      style={{
+        paddingTop: "2rem",
+        paddingBottom: "2rem",
+      }}
+    >
+      <h2 style={{ textAlign: "center", marginBottom: "2rem" }}>{title} </h2>
+      <Row style={{ fontSize: "2rem" }}>
+        {icons.map((icon, index) => (
+          <Col key={index}>
+            <div style={cardStyles}>
+              {icon}
+              <BsDashLg size={60} />
+              <Counter number={numbers[index]} />
+              <span>{texts[index]}</span>
+            </div>
           </Col>
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <BsFillTrophyFill size={40} />
-            <BsDashLg size={40} />
-            <Counter number={7900} /> {/*число на счетчике */}
-            <span>enim esse</span>
-          </Col>
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <BsFillMortarboardFill size={40} />
-            <BsDashLg size={40} />
-            <Counter number={30} /> {/*число на счетчике */}
-            <span>enim esse</span>
-          </Col>
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-            }}
-          >
-            <BsFillHandThumbsUpFill size={40} />
-            <BsDashLg size={40} />
-            <Counter number={20} /> {/*число на счетчике */}
-            <span>enim esse</span>
-          </Col>
-        </Row>
-      </Container>
-    </>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
